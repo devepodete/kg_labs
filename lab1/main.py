2f406fab940bd33678ab6ffe33a724f5fe0be668
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2020-10-06 18:32:15 +0300

    Update main.py
    X and Y axes added

--- lab1/main.py
+++ lab1/main.py
@@ -7,6 +7,7 @@ from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from mpl_toolkits.axisartist import SubplotZero
 
+import random
 
 class App(tk.Tk):
     def __init__(self):
@@ -14,9 +15,9 @@ class App(tk.Tk):
         self.initialize()
 
     def initialize(self):
-        self.title("A Matplotlib graph inside a Tkinter Python GUI app")
+        self.title('Some title name')
 
-        button = tk.Button(self, text="Quit", command=self.on_click)
+        button = tk.Button(self, text='Quit', command=self.on_click)
         button.pack(side=tk.BOTTOM)
 
         self.a = tk.DoubleVar()
@@ -26,31 +27,43 @@ class App(tk.Tk):
         self.B = tk.DoubleVar()
         self.B.set(18.5)
 
-        padding = 100
-
         slider_a = tk.Scale(self, from_=10, to=1, resolution=0.1,
                             label='a', variable=self.a,
                             command=self.on_change)
         slider_a.pack(side=tk.LEFT, padx = 0, fill=None, expand=False)
+
         slider_k = tk.Scale(self, from_=1, to=-1, resolution=0.01,
                             label='k', variable=self.k,
                             command=self.on_change)
         slider_k.pack(side=tk.LEFT, padx = 0, fill=None)
-        slider_B = tk.Scale(self, from_=30, to=0, resolution=0.1,
+
+        slider_B = tk.Scale(self, from_=30, to=1, resolution=0.1,
                             label='B', variable=self.B,
                             command=self.on_change)
         slider_B.pack(side=tk.LEFT, padx = 0, fill=None)
 
 
         x, y = self.data(self.a.get(), self.k.get(), self.B.get())
-        fig, ax = self.configure_plot(x, y)
+        self.fig, self.ax = self.configure_plot(x, y)
 
-        self.line1, = ax.plot(x, y)
+        xBorder, yBorder = self.calculate_borders(x, y)
 
-        self.canvas = FigureCanvasTkAgg(fig, master=self)
+        xlim = (-xBorder, xBorder)
+        ylim = (-yBorder, yBorder)
+
+        self.change_limits(xlim, ylim)
+
+        self.draw_text(xBorder, yBorder)
+
+        self.line1, = self.ax.plot(x, y, color=random.choice(['red', 'green', 'blue', 'black', 'orange']))
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         toolbar = NavigationToolbar2Tk(self.canvas, self)
+        toolbar.update()
+
+        self.canvas.draw()
 
 
     def on_click(self):
@@ -58,24 +71,44 @@ class App(tk.Tk):
 
     def on_change(self, value):
         x, y = self.data(self.a.get(), self.k.get(), self.B.get())
+        xBorder, yBorder = self.calculate_borders(x, y)
+
+        xlim = (-xBorder, xBorder)
+        ylim = (-yBorder, yBorder)
+
+        self.change_limits(xlim, ylim)
+        self.line1.set_data(x, y)
+
+        for i in range(len(self.ax.texts)):
+            self.ax.texts[i].set_visible(False)
+
+        self.draw_text(xBorder, yBorder)
+
+        self.canvas.draw()
+
+    @staticmethod
+    def change_limits(xlim, ylim):
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+
+    @staticmethod
+    def calculate_borders(x, y):
         xmin = -10
         xmax = 10
         ymin = 0
         ymax = 10
-        xBorder = max(abs(min(min(x),xmin)), abs(max(max(x),xmax)))
+        xBorder = max(abs(min(min(x), xmin)), abs(max(max(x), xmax)))
         yBorder = max(abs(min(min(y), ymin)), abs(max(max(y), ymax)))
-        xlim = (-xBorder, xBorder)
-        ylim = (-yBorder, yBorder)
-        self.change_limits(xlim, ylim)
-        self.line1.set_data(x, y)
-        self.canvas.draw()
 
-    def change_limits(self, xlim, ylim):
-        plt.xlim(xlim)
-        plt.ylim(ylim)
+        return xBorder, yBorder
 
-    def configure_plot(self, x, y):
-        fig = plt.figure()
+    def draw_text(self, xBorder, yBorder):
+        self.ax.text(xBorder, yBorder / 20, 'X', size=10, bbox=dict(facecolor='orange', alpha=0.3))
+        self.ax.text(xBorder / 20, yBorder, 'Y', size=10, bbox=dict(facecolor='orange', alpha=0.3))
+
+    @staticmethod
+    def configure_plot(x, y):
+        fig = plt.figure(1)
         plt.style.use('seaborn-bright')
         ax = SubplotZero(fig, 111)
         fig.add_subplot(ax)
@@ -89,11 +122,11 @@ class App(tk.Tk):
 
         plt.grid()
         plt.title('$ae^{k\Phi}$')
-        plt.tight_layout(pad=0.6)
 
         return fig, ax
 
-    def data(self, a, k, B):
+    @staticmethod
+    def data(a, k, B):
         Phi = np.linspace(0, B, int(B)*5)
         r = a * np.exp(k * Phi)
 
