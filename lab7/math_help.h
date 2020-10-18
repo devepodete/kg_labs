102249645315fb479f2dced60ee9db5cc40a3e0d
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2020-10-18 11:53:11 +0300

    math function moved to separated file. + bug fixed
    In linspace(left, right, count) function due to comparison of floating point numbers, the right number was not always included in the answer

---
+++ lab7/math_help.h
@@ -0,0 +1,59 @@
+//
+// Created by zero on 18/10/2020.
+//
+
+#pragma once
+
+
+#include <vector>
+#include <algorithm>
+
+
+namespace math {
+
+    //return evenly spaced numbers over a specified interval
+    std::vector<float> linspace(float t1, float t2, unsigned count = 100) {
+        if (count == 0) {
+            return {};
+        }
+
+        if (count == 1) {
+            return {t1};
+        }
+
+        if (count == 2) {
+            return {t1, t2};
+        }
+
+        bool reverse = false;
+        if (t1 > t2) {
+            std::swap(t1, t2);
+            reverse = true;
+        }
+
+        std::vector<float> res(0);
+
+        float distance = t2 - t1;
+        float step = distance / static_cast<float>(count - 1);
+        //std::cout << "distance: " << distance << std::endl;
+        //std::cout << "step: " << step << std::endl;
+
+        float cur = t1;
+        while (cur <= t2 || std::abs(cur - t2) < 1e-5) {
+            res.push_back(cur);
+            cur += step;
+            //std::cout << "cur = " << cur << std::endl;
+            //std::cout << "std::abs(cur - t2) = " << std::abs(cur - t2) << std::endl;
+        }
+
+        if (res.size() < count) {
+            res.push_back(t2);
+        }
+
+        if (reverse) {
+            std::reverse(res.begin(), res.end());
+        }
+        return res;
+    }
+
+} //namespace math
