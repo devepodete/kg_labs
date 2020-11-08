5e0e756b81c6be61375d9a8e4cdde38132f380bd
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2020-11-08 20:03:32 +0300

    3d

--- lab45/main.cpp
+++ lab45/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 // do not use nullptr in OpenGL functions
 // #define nullptr NULL
@@ -15,6 +20,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 std::vector<char> shader_source_code(const std::string &filename);
 GLuint createShader(GLuint shaderType, const std::string &file);
 GLuint attachAndLinkShaders(GLuint vertexShader, GLuint fragmentShader);
+
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
@@ -42,43 +48,101 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+    std::vector<float> vertices = {
+            // positions         // colors
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    // top left
+            0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top right
     };
 
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    float vertices1[] = {
+            // positions         // colors
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    // top left
+            0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top right
+    };
+
+
+    unsigned int indices[] = {
+            1, 0, 2,
+            2, 0, 3
+    };
+
 
     GLuint vertexShader = createShader(GL_VERTEX_SHADER, "../shaders/shader.vert");
     GLuint fragmentShader = createShader(GL_FRAGMENT_SHADER, "../shaders/shader.frag");
     GLuint shaderProgram = attachAndLinkShaders(vertexShader, fragmentShader);
-    glUseProgram(shaderProgram);
-
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     std::cout << "--- SHADERS INITIALIZED ---\n";
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    GLuint VAO1, VBO1, EBO1;
+    glGenVertexArrays(1, &VAO1);
+    glGenBuffers(1, &VBO1);
+    glGenBuffers(1, &EBO1);
+    glBindVertexArray(VAO1);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size(), vertices.data(), GL_DYNAMIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
+    glUseProgram(shaderProgram);
 
-    
+    glCullFace(GL_CW);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
 
-
-    glViewport(0, 0, WIDTH, HEIGHT);
-
+    unsigned i = 0;
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_DYNAMIC_DRAW);
+        glBindVertexArray(VAO1);
+
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0, 0.0, 0.0));
+        model = glm::scale(model, glm::vec3(0.7, 0.5, 0.5));
+
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH/(float)HEIGHT, 0.1f, 100.0f);
+
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+
+        auto timeValue = (float)glfwGetTime();
+        float white = std::sin(timeValue) / 2.0f + 0.5f;
+        std::cout << white << std::endl;
+        glUniform1f(glGetUniformLocation(shaderProgram, "coeff"), white);
+
 
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO1);
+    glDeleteBuffers(1, &VBO1);
+    glDeleteBuffers(1, &EBO1);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 
@@ -123,6 +187,7 @@ std::vector<char> shader_source_code(const std::string &filename) {
 
 GLuint createShader(GLuint shaderType, const std::string &file) {
     std::vector<char> shaderData = shader_source_code(file);
+    shaderData.push_back('\0');
     const char *shaderSource = shaderData.data();
 
     GLuint shaderID;
